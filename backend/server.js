52c6ec815ebffbc158fcd0a0f52c9c302172059f
lejlasa <lejla.saric@bhtelecom.ba>
2024-03-21 09:52:04 +0100

    dodani back i front

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const morgan = require('morgan');
+require('dotenv').config();
+
+const app = express(),
+  port=process.env.PORT||3005;
+
+  app.use(morgan('combined'));
+
+// Pokretanje servera
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
+const dbService = require('./services/db-service');
+dbService.initOracleClient();
+dbService.connectToDatabase();
+
+app.get('/', (req,res)=>{
+  res.send("Hello");
+})
